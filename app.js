7a0ea6846265b0622b0cf4c562392013d290cb03
kssandhu1 <sandhuka@oregonstate.edu>
2024-06-02 04:22:15 -0400

    Created a dictionary controller/route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose";
 import convertRouter from "./controllers/conversion.js";
+import dictionRouter from "./controllers/dictionary.js";
 
 mongoose.set("strictQuery", false);
 
@@ -24,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", convertRouter);
+app.use("/", dictionRouter);
 
 // app.get("/", (request, response) => {
 //   response.send("<h1>Hello World!</h1>");
---
+++ controllers/dictionary.js
@@ -0,0 +1,18 @@
+import express, { json } from "express";
+import "express-async-errors";
+import Word from "../models/dictionaryWord.js";
+
+const dictionRouter = express.Router();
+
+dictionRouter.get("/dictionary", async (req, res) => {
+  let allWords = await Word.find({});
+  Array.prototype.sortBy = function (p) {
+    return this.slice(0).sort(function (a, b) {
+      return a[p] > b[p] ? 1 : a[p] < b[p] ? -1 : 0;
+    });
+  };
+  allWords = allWords.sortBy("phonetic");
+  res.send("allWords");
+});
+
+export default dictionRouter;
