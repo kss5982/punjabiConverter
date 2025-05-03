347cc1ffc4bbf3b5fc21ea5095e720621d9db707
kssandhu1 <sandhuka@oregonstate.edu>
2025-05-03 02:18:55 -0400

    Fixed dropdown and |/|| logic

--- controllers/conversion.js
+++ controllers/conversion.js
@@ -43,41 +43,57 @@ convertRouter.post("/", async (req, res) => {
   const fuse = new Fuse(convertedArrayObj.flat(1), options);
   // console.log(fuse);
   // iterates through original phonetic array and finds the correct conversion from returned array of objects
-  // convertedArray will be used in REGEX (first value of dropdown)
-  let convertedArray = [];
+  // finalArray will be used in REGEX (first value of dropdown)
+  let finalArray = [];
   let finalDropDownMenu = [];
   for (const phoneticWord of phoneticTextArr) {
     let exactExists;
     let initialSearch = convertedArrayObj
       .flat(1)
       .find((word) => word.phonetic === phoneticWord);
-    console.log("converted word object:", initialSearch);
+    // console.log("converted word object:", initialSearch);
     if (initialSearch) {
       exactExists = true;
     }
+    // creates an array with the best fuzzy match converted values + phonetic word
+    let dropDownValues = [];
+    if (exactExists && !punctuation.includes(phoneticWord)) {
+      console.log(`exact match! ${phoneticWord} exists in dictionary`);
+      finalArray.push(initialSearch.converted[0]);
+      // checks if converted word has more than 1 possible value
+      if (initialSearch.converted.length > 1) {
+        // if it does, then add 2nd possible value and onwards into dropdown menu (don't need first)
+        dropDownValues = Array.from(initialSearch.converted.slice(1));
+        console.log(dropDownValues);
+      }
+      dropDownValues.push(phoneticWord);
+      // console.log(dropDownValues);
+      finalDropDownMenu.push(dropDownValues);
+    }
     // console.log( convertedArrayObj);
     // if exact value doesn't match from DB, perform second fuzzy search
-    if (!punctuation.includes(phoneticWord)) {
-      let convertedWord = fuse.search(phoneticWord);
+    else if (!punctuation.includes(phoneticWord)) {
+      let fuseWord = fuse.search(phoneticWord);
+      // console.log("fuse word", fuseWord);
       // console.log("2nd fuzzy search", phoneticWord);
       // check if 2nd fuzzy search has content
-      if (convertedWord.length > 0) {
-        //convertedWord[0].item.converted[0] is the array of coverted values
-        convertedArray.push(convertedWord[0].item.converted[0]);
-        // console.log("converted array: ", convertedArray);
+      if (fuseWord.length > 0) {
+        //fuseWord[0].item.converted[0] is the array of coverted values
+        finalArray.push(fuseWord[0].item.converted[0]);
+        // console.log("final array: ", finalArray);
 
-        // creates an array with the best fuzzy match converted values + phonetic word
-        let dropDownValues = [];
         // checks if converted word has more than 1 possible value
-        if (convertedWord[0].item.converted.length > 1) {
+        if (fuseWord[0].item.converted.length > 1) {
           // if it does, then add 2nd possible value and onwards into dropdown menu (don't need first)
-          dropDownValues = Array.from(convertedWord[0].item.converted.slice(1));
+          dropDownValues = Array.from(fuseWord[0].item.converted.slice(1));
           console.log(dropDownValues);
         }
-        // add additional dropdown suggestions
-        if (fuse._docs[1] && !exactExists) {
-          // console.log(fuse._docs);
-          dropDownValues.push(...fuse._docs[1].converted);
+        // add additional non-duplicate dropdown suggestions
+        if (
+          fuseWord.length > 1 &&
+          !fuseWord[1].item.converted.includes(fuseWord[0].item.converted[0])
+        ) {
+          dropDownValues.push(...fuseWord[1].item.converted);
           console.log("dropdown values", dropDownValues);
         }
         dropDownValues.push(phoneticWord);
@@ -93,19 +109,19 @@ convertRouter.post("/", async (req, res) => {
         } else if (new RegExp(emojiPattern, "g").test(phoneticWord)) {
           finalDropDownMenu.push([phoneticWord]);
         }
-        convertedArray.push(phoneticWord);
+        finalArray.push(phoneticWord);
       }
-      // console.log(convertedArray);
+      // console.log(finalArray);
       // adds punctuation back into the array
-    } else convertedArray.push(phoneticWord);
-    // console.log(convertedWord);
+    } else finalArray.push(phoneticWord);
+    // console.log(fuseWord);
   }
-  // console.log("converted array: ", convertedArray);
+  // console.log("converted array: ", finalArray);
   // console.log(finalDropDownMenu);
-  let finalText = convertedArray.join(" ");
+  let finalText = finalArray.join(" ");
   // console.log("before regex", finalText);
   finalText = finalText
-    .replace(/\s(?=!|\?|\.|:|,|\)|\]|\}|@|%|\^|\*|\+|_|~|\/|\\|l|I|\|)/g, "") // removes space before character
+    .replace(/\s(?=!|\?|\.|:|,|\)|\]|\}|@|%|\^|\*|\+|_|~|\/|\\|\|)/g, "") // removes space before character
     .replace(/(?<=\(|\{|\[|#|\$|'|`|_|\n)\s/g, "") // removes space after character
     .replace(/" *([^"]*?) *"/g, '"$1"'); // removes spaces between quotes
 
