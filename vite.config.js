8d3fc6196bb9d9284af2204a9ccc15d6e7189026
kssandhu1 <sandhuka@oregonstate.edu>
2026-01-05 01:16:47 -0500

    More google analytics shit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import csp from "vite-plugin-csp-guard";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    csp({
+      algorithm: "sha256", // The algorithm to use for hashing
+      dev: {
+        run: true, // If you want to run the plugin in `vite dev` mode
+      },
+      policy: {
+        // Specify the policy here.
+        "script-src-elem": [
+          "'self'",
+          "https://cdn.jsdelivr.net",
+          "https://www.googletagmanager.com/gtag/",
+        ],
+        "script-src": [
+          "'self'",
+          "https://cdn.jsdelivr.net",
+          "https://www.googletagmanager.com",
+        ], // Example: Allow Google Analytics
+        "connect-src": ["https://cdn.jsdelivr.net"],
+        "style-src-elem": ["'self'", "https://cdn.jsdelivr.net"], // Allow bootstrap stylesheet
+      },
+    }),
+  ],
   server: {
     proxy: {
       "/": {
